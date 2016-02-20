ad6c049b49effdbf4e1986384d4470cfe3c7cb07
Jay Shenoy <jayshenoy@hotmail.com>
2016-02-20 00:54:26 -0500

    Initial Commit

---
+++ Parkinsons App ClientTests/Parkinsons_App_ClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  Parkinsons_App_ClientTests.swift
+//  Parkinsons App ClientTests
+//
+//  Created by Jay Shenoy on 2/20/16.
+//  Copyright © 2016 FP Computer Club. All rights reserved.
+//
+
+import XCTest
+@testable import Parkinsons_App_Client
+
+class Parkinsons_App_ClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
